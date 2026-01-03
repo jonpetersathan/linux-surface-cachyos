b4211e5bb1e60537eb406013c1283abcb0e6403f
jonpetersathan <jonpetersathan@gmail.com>
2026-01-03 19:00:58 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+## Enable ZFS
+#find . -name "PKGBUILD" | xargs -I {} sed -i "s/_build_zfs:=no/_build_zfs:=yes/" {}
+## Enable Generic v3
+find . -name "PKGBUILD" | xargs -I {} sed -i "s/_processor_opt:=/_processor_opt:=GENERIC_V3/" {}
+find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_auto_optimization:=yes/_use_auto_optimization:=no/" {}
+## Enable Open NVIDIA module
+#find . -name "PKGBUILD" | xargs -I {} sed -i "s/_build_nvidia_open:=no/_build_nvidia_open:=yes/" {}
+## Disable clang-LTO
+find . -name "PKGBUILD" | xargs -I {} sed -i "s/_use_llvm_lto:=thin/_use_llvm_lto:=none/" {}
+
+## GCC v3 Kernel
+files=$(find . -name "PKGBUILD")
+
+for f in $files
+do
+    d=$(dirname $f)
+    cd $d
+    time docker run --name kernelbuild -e EXPORT_PKG=1 -e SYNC_DATABASE=1 -e CHECKSUMS=1 -v $PWD:/pkg pttrr/docker-makepkg-v3
+    docker rm kernelbuild
+    cd ..
+done
